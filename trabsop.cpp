3de8e591bdbb83164886213dbb4241a5d931ef3c
anacarolinatorejani <ana.torejani@unesp.br>
2025-03-18 15:15:56 -0300

    Delete trabsop.cpp

--- trabsop.cpp
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-main()
-{
-	float tempc;
-	printf("Digite a temperatura: ");
-	scanf("%f", &tempc);
-	float tempf = (tempc * 9/5) + 32;
-	printf("A temperatura de Celsius para Fahrenheit: %.2f", tempf);
-}
